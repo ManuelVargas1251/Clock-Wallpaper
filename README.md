a63d83d2c187a548e27333086f5fd78c690643f7
Manuel Vargas <manuelvargas@my.unt.edu>
2017-04-20 13:10:20 -0500

    1.2.2
    fixed noon being undefined and added toggle to hide details

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-![Version](https://img.shields.io/badge/Version-1.2.1-blue.svg)
+![Version](https://img.shields.io/badge/Version-1.2.2-blue.svg)
 # Clock + Wallpaper
 Simple clock with random image using [Unsplash API](https://source.unsplash.com/). Just something nice to have in the background. Might add options for font color picker and font size.
+
+## Features
+* Click anywhere on screen to hide details
 
 ## Issues
 Newest Unsplash API makes you wait a certain amount of time before you can request a different picture from them so please wait a few seconds before reloading to the next image.
--- time.js
+++ time.js
@@ -1,7 +1,7 @@
-function checkTime(i) {
-	if (i < 10) 
-		i = "0" + i	// add zero in front of numbers < 10
-	return i
+function checkTime(m) {
+	if (m < 10) 
+		m = "0" + m	// add zero in front of minutes < 10
+	return m
 }
 function twelvehour(h) {
 	if (h>12){
@@ -16,25 +16,56 @@ function twelvehour(h) {
 	else	
 		return h
 }
-//return post or ante
+//return post or ante depending on time of day
 function get_ampm(h){
 	if (h<12)
 		return "AM"
-	if (h>12)
+	if (h>=12)
 	   return "PM"
 }
 function start() {
 	var today = new Date()
 	var h = today.getHours()
 	var m = today.getMinutes()
+	//console.log("h: "+h)
 	var ampm = get_ampm(h)
 
 	//clean both sources
-	m = checkTime(m)	//add 0 for nums > 10
+	m = checkTime(m)	//add leading 0 for minutes > 10
 	h = twelvehour(h)	//changes to 12hr format
 
 	//write time to screen
-    document.getElementById('txt').innerHTML = h + ":" + m + " " + ampm
+    document.getElementById('time').innerHTML = h + ":" + m + " " + ampm
 	//Since we aren't printing seconds, we can wait longer to call the function again
     var t = setTimeout(start, 2*1000) //every 2 seconds
+}
+
+var _hide_details = false
+var count = 0
+
+//hides details when toggled
+function toggle(){
+	//console.log(":toggle initiated:")
+	var info = document.getElementById('info');
+	
+	//toggle the state
+	if(_hide_details == false)
+		_hide_details = true
+	else
+		_hide_details = false
+	
+	
+	//if true and if the counter is even, hide info
+	if(_hide_details == true && count%2==0){
+		info.style.display = 'none'
+		//console.log("Hiding Info")
+	}
+	else{
+		info.style.display = 'block'
+		//console.log("Showing Info")
+	}
+		
+	
+	count++
+	//console.log(":complete:"+_hide_details)
 }
